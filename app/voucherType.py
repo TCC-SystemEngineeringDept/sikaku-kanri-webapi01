ae4315959b66f5699f7fbf4f9f674e9517bfb4d6
JC-21\210783 <210783@jc-21.jp>
2024-01-19 15:34:12 +0900

    追加処理とlist変更しました

--- app/voucherType.py
+++ app/voucherType.py
@@ -11,7 +11,8 @@ voucherTypes = [
 
 @app.get("/list")
 def get_voucher_type_list(token:str):
-    return voucherTypes
+    vname = db.query(db.VoucherType).all()
+    return vname
 
 @app.get("/{ID}")
 def get_voucher_type_item(ID:str,token:str):
@@ -22,18 +23,13 @@ def get_voucher_type_item(ID:str,token:str):
     else:
         return {}
 
-@app.get("/{NAME}")
-def get_voucher_type_item(NAME:str,token:str):
-    vname = db.query(db.VoucherType.voucher_name).all()
-    if vname == "FE/SG受験バウチャー":
-        return vname[0]
-    elif vname == "Oracle認定資格ピアソンVUE 配信監督なし試験用":
-        return vname[1]
-    else:
-        return{}  
-
-
 @app.post("/add")
 def get_voucherType_item(ID:str,NAME:str,token:str):
-    return {"message": "voucherType was not added successfully", "voucherType": {}}
+    if(ID == None or NAME == None):
+        return {"message": "voucherType was not added successfully", "voucherType": {}}
+    else:
+        db.VoucherType.voucher_id.add(ID)
+        db.VoucherType.voucher_name.add(NAME)
+        return{"追加処理成功!!!!!":(ID,NAME)}
+    
 	
