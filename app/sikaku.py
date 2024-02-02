e6c85cc8be55296620d87f000e57411a81c8f888
JC-21\210814 <210814@jc-21.jp>
2024-02-02 15:16:59 +0900

    確認のための変更

--- app/sikaku.py
+++ app/sikaku.py
@@ -21,8 +21,8 @@ def get_db():
 @app.get("/list")
 def get_passed_list(token:str, db: Session = Depends(get_db)):
     joined_table = db.query(Sikaku, Sikaku.exam_id, Exam.exam_name, Sikaku.passed_date).join(Exam, Sikaku.exam_id == Exam.exam_id).all()
-    result_dict = [u.__dict__ for u in joined_table]
-    return result_dict
+    print(joined_table, type(joined_table))
+    return {}
 
 @app.get("/{ID}")
 def get_passed_item(ID:str,token:str, db: Session = Depends(get_db)):
